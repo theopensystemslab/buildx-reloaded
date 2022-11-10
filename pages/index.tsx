6f51279bb11292f138a5c078e8b459d4c4f9e63a
Thomas O'Neill <tom@opensystemslab.io>
2022-11-10 11:44:23 +0000

    for collisions preview (with vertical)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
 import DataInit from "@/data/DataInit"
 import AppInit from "@/ui-3d/init/AppInit"
 import { Fragment } from "react"
-import GltfApp from "../src/ui-3d/gltf/GltfApp"
+import BoxApp from "../src/ui-3d/box/BoxApp"
 
 const IndexPage = () => {
   return (
     <Fragment>
       <DataInit />
       <AppInit>
-        {/* <BoxApp />
-        <DebugApp /> */}
-        {/* <InstancedApp /> */}
-        <GltfApp />
+        <BoxApp />
       </AppInit>
     </Fragment>
   )
