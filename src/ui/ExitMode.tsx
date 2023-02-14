f1176cb2367010d60c7b20936cf227067f38336e
Thomas O'Neill <tom@opensystemslab.io>
2023-02-14 12:02:49 -0800

    exit button styles

--- src/ui/ExitMode.tsx
+++ src/ui/ExitMode.tsx
@@ -1,6 +1,6 @@
+import { Close } from "@carbon/icons-react"
 import usePortal from "react-cool-portal"
 import { SiteCtxModeEnum, upMode, useSiteCtx } from "../hooks/siteCtx"
-import { Check } from "./icons"
 
 const ExitMode = () => {
   const { mode } = useSiteCtx()
@@ -12,13 +12,15 @@ const ExitMode = () => {
 
   return mode === SiteCtxModeEnum.Enum.SITE ? null : (
     <Portal>
-      <div className="absolute left-1/2 top-16 z-50 flex -translate-x-1/2 transform justify-center">
+      <div className="absolute left-1/2 top-32 z-50 flex -translate-x-1/2 transform justify-center">
         <button
           onClick={upMode}
-          className="block h-12 w-12 rounded-full bg-white p-2 text-green-500 shadow-lg hover:bg-grey-10"
+          className="flex justify-between items-center h-12 rounded-full bg-white p-2 text-green-500 shadow-lg hover:bg-grey-10"
         >
-          <span>{`Exit ${mode.toLowerCase()} mode?`}</span>
-          <Check />
+          <span className="ml-6 mr-2">{`Exit ${mode.toLowerCase()}`}</span>
+          <span className="w-8 p-1">
+            <Close size="24" />
+          </span>
         </button>
       </div>
     </Portal>
