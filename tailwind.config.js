5cbc32bda7d5a9f3808a5e865270a2efdc8d239e
Thomas O'Neill <tom@opensystemslab.io>
2023-03-16 05:42:32 -0700

    wip tailwind colors fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**.{js,ts,jsx,tsx}",
-    "./pages/**.{js,ts,jsx,tsx}",
+    "./app/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
