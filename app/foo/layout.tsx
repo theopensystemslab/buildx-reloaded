48d3bbbb6cfb171c39c2ba7a6cff9a619820ebef
Thomas O'Neill <tom@opensystemslab.io>
2023-03-22 11:53:46 -0700

    wip fixup, cleanup

--- app/foo/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { TrpcProvider } from "../common/TrpcProvider"
-
-const FooLayout = ({ children }: any) => {
-  return <TrpcProvider>{children}</TrpcProvider>
-}
-
-export default FooLayout
