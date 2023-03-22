2e47273c7e4e19c5f8b8bcf2eb287936b345b5fc
Thomas O'Neill <tom@opensystemslab.io>
2023-03-22 12:23:42 -0700

    try cors

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,12 +1,18 @@
 import { initTRPC } from "@trpc/server"
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch"
+import { NextApiResponse } from "next"
 import { appRouter } from "../../../../server/trpc/router"
 
 const t = initTRPC.create()
 
 export type AppRouter = typeof appRouter
 
-const handler = (request: Request) => {
+const handler = async (request: Request, response: NextApiResponse) => {
+  response.setHeader("Access-Control-Allow-Origin", "*")
+  response.setHeader("Access-Control-Request-Method", "*")
+  response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET")
+  response.setHeader("Access-Control-Allow-Headers", "*")
+
   return fetchRequestHandler({
     endpoint: "/api/trpc",
     req: request,
