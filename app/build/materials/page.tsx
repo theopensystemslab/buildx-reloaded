14fc3b79444989762855a4f117a5de40928ed129
mysterybear <50906986+mysterybear@users.noreply.github.com>
2023-04-06 16:21:51 +0100

    feat: build overview markup (#21)
    Co-authored-by: Thomas O'Neill <tom@opensystemslab.io>

--- app/build/materials/page.tsx
+++ app/build/materials/page.tsx
@@ -5,11 +5,14 @@ import MaterialsListTable from "./MaterialsListTable"
 const MaterialsListIndexPage = () => {
   return (
     <div>
-      <h1>Materials list</h1>
-      <p>
-        A list of the other materials you will need to find and purchase from
-        other manufacturers. All prices are estimated.
-      </p>
+      <div className="px-3 py-5">
+        <h1>Materials list</h1>
+
+        <p className="max-w-3xl mt-2">
+          A list of the other materials you will need to find and purchase from
+          other manufacturers. All prices are estimated.
+        </p>
+      </div>
       <MaterialsListTable />
     </div>
   )
