243eb4795ab1a2177f7eb53fce02f3d10c85ba97
Thomas O'Neill <tom@opensystemslab.io>
2023-04-27 06:25:17 -0600

    wip polygon with distances

---
+++ app/locate/mapStyles.ts
@@ -0,0 +1,70 @@
+export const polygonDrawStyles = [
+  // polygon fill
+  {
+    id: "gl-draw-polygon-fill",
+    type: "fill",
+    filter: ["all", ["==", "$type", "Polygon"], ["!=", "mode", "static"]],
+    paint: {
+      "fill-color": "rgba(255, 255, 255, 0.5)",
+      "fill-outline-color": "#ffffff",
+    },
+  },
+  // polygon outline stroke
+  {
+    id: "gl-draw-polygon-stroke-active",
+    type: "line",
+    filter: ["all", ["==", "$type", "Polygon"], ["!=", "mode", "static"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": "#ffffff",
+      "line-width": 2,
+    },
+  },
+  // vertex point halos
+  {
+    id: "gl-draw-polygon-and-line-vertex-halo-active",
+    type: "circle",
+    filter: [
+      "all",
+      ["==", "meta", "vertex"],
+      ["==", "$type", "Point"],
+      ["!=", "mode", "static"],
+    ],
+    paint: {
+      "circle-radius": 5,
+      "circle-color": "#ffffff",
+    },
+  },
+  // vertex points
+  {
+    id: "gl-draw-polygon-and-line-vertex-active",
+    type: "circle",
+    filter: [
+      "all",
+      ["==", "meta", "vertex"],
+      ["==", "$type", "Point"],
+      ["!=", "mode", "static"],
+    ],
+    paint: {
+      "circle-radius": 3,
+      "circle-color": "#ffffff",
+    },
+  },
+  // uncommitted line
+  {
+    id: "gl-draw-uncommitted-line",
+    type: "line",
+    filter: ["all", ["==", "$type", "LineString"], ["!=", "mode", "static"]],
+    layout: {
+      "line-cap": "round",
+      "line-join": "round",
+    },
+    paint: {
+      "line-color": "#ffffff",
+      "line-width": 2,
+    },
+  },
+]
