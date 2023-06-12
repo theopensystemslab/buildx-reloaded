ca674c7404dfdd27fcaa4dcc93afc021ba78fe77
Thomas O'Neill <tom@opensystemslab.io>
2023-06-12 09:39:41 +0100

    wip broken gltf exporter

--- app/design/state/exporters/index.ts
+++ app/design/state/exporters/index.ts
@@ -3,42 +3,47 @@ import { useEffect, useRef } from "react"
 import { useEvent } from "react-use"
 import { ExportersWorkerAPI } from "./worker"
 
-export const UPDATE_OBJ_EVENT = "UpdateOBJ"
-export const GET_OBJ_EVENT = "GetOBJ"
+export const UPDATE_EXPORT_MODELS_EVENT = "UpdateExportModels"
+export const GET_EXPORT_MODEL_EVENT = "GetExportModel"
 
-export type UpdateOBJEventDetail = {
+export type UpdateWorkerGroupEventDetail = {
   houseId: string
   payload: any
 }
 
-export type GetOBJEventDetail = {
+export type UpdateWorkerGroupEvent = {
+  type: typeof UPDATE_EXPORT_MODELS_EVENT
+  detail: UpdateWorkerGroupEventDetail
+}
+
+export type GetModelEventDetail = {
   houseId: string
+  format: "OBJ" | "GLB"
 }
 
-export type UpdateOBJEvent = {
-  type: typeof UPDATE_OBJ_EVENT
-  detail: UpdateOBJEventDetail
+export type GetModelEvent = {
+  type: typeof GET_EXPORT_MODEL_EVENT
+  detail: GetModelEventDetail
 }
 
-export type GetOBJEvent = {
-  type: typeof GET_OBJ_EVENT
-  detail: GetOBJEventDetail
-}
-
-export const dispatchUpdateOBJEvent = ({
+export const dispatchUpdateExportModelsEvent = ({
   houseId,
   payload,
-}: UpdateOBJEventDetail) => {
-  console.log({ payload })
+}: UpdateWorkerGroupEventDetail) => {
   dispatchEvent(
-    new CustomEvent(UPDATE_OBJ_EVENT, {
+    new CustomEvent(UPDATE_EXPORT_MODELS_EVENT, {
       detail: { houseId, payload },
     })
   )
 }
 
-export const dispatchGetOBJEvent = ({ houseId }: GetOBJEventDetail) => {
-  dispatchEvent(new CustomEvent(GET_OBJ_EVENT, { detail: { houseId } }))
+export const dispatchGetModelEvent = ({
+  houseId,
+  format,
+}: GetModelEventDetail) => {
+  dispatchEvent(
+    new CustomEvent(GET_EXPORT_MODEL_EVENT, { detail: { houseId, format } })
+  )
 }
 
 export const useOBJExporterWorker = () => {
@@ -51,29 +56,59 @@ export const useOBJExporterWorker = () => {
   }, [])
 
   useEvent(
-    UPDATE_OBJ_EVENT,
-    ({ detail: { houseId, payload } }: UpdateOBJEvent) => {
-      ref.current?.updateOBJ({ houseId, payload })
+    UPDATE_EXPORT_MODELS_EVENT,
+    ({ detail: { houseId, payload } }: UpdateWorkerGroupEvent) => {
+      ref.current?.updateModels({ houseId, payload })
     }
   )
 
-  useEvent(GET_OBJ_EVENT, async ({ detail: { houseId } }: GetOBJEvent) => {
-    const objData = await ref.current?.getOBJ(houseId)
-    if (!objData) return
+  useEvent(
+    GET_EXPORT_MODEL_EVENT,
+    async ({ detail: { houseId, format } }: GetModelEvent) => {
+      switch (format) {
+        case "GLB": {
+          const gltfData = await ref.current?.getGLB(houseId)
+          if (!gltfData) return
 
-    const blob = new Blob([objData], { type: "text/plain" }) // Create a Blob from the OBJ data
-    const url = URL.createObjectURL(blob) // Create a URL for the Blob
+          const blob = new Blob([gltfData], { type: "model/gltf-binary" }) // Create a Blob from the OBJ data
+          const url = URL.createObjectURL(blob) // Create a URL for the Blob
 
-    // Create a download link and click it
-    const link = document.createElement("a")
-    link.href = url
-    link.download = "model.obj" // Set the file name
-    link.style.display = "none"
-    document.body.appendChild(link)
-    link.click()
+          // Create a download link and click it
+          const link = document.createElement("a")
+          link.href = url
+          link.download = "model.glb" // Set the file name
+          link.style.display = "none"
+          document.body.appendChild(link)
+          link.click()
 
-    // Cleanup
-    document.body.removeChild(link)
-    URL.revokeObjectURL(url)
-  })
+          // Cleanup
+          document.body.removeChild(link)
+          URL.revokeObjectURL(url)
+          return
+        }
+        case "OBJ": {
+          const objData = await ref.current?.getOBJ(houseId)
+          if (!objData) return
+
+          const blob = new Blob([objData], { type: "text/plain" }) // Create a Blob from the OBJ data
+          const url = URL.createObjectURL(blob) // Create a URL for the Blob
+
+          // Create a download link and click it
+          const link = document.createElement("a")
+          link.href = url
+          link.download = "model.obj" // Set the file name
+          link.style.display = "none"
+          document.body.appendChild(link)
+          link.click()
+
+          // Cleanup
+          document.body.removeChild(link)
+          URL.revokeObjectURL(url)
+          return
+        }
+        default:
+          return
+      }
+    }
+  )
 }
