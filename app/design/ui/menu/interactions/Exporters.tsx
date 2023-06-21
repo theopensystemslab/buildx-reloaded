4429cf85a086c7ee82837c70f6ae7121f8f09f0c
Thomas O'Neill <tom@opensystemslab.io>
2023-06-21 15:49:12 +0100

    wip getting blocks in a worker

--- app/design/ui/menu/interactions/Exporters.tsx
+++ app/design/ui/menu/interactions/Exporters.tsx
@@ -1,5 +1,5 @@
 import { ArrowDown } from "@carbon/icons-react"
-import { dispatchGetModelEvent } from "../../../state/exporters"
+import { dispatchGetModelEvent } from "../../../workers/exporters"
 import ContextMenuButton from "../ContextMenuButton"
 import ContextMenuNested from "../ContextMenuNested"
 
