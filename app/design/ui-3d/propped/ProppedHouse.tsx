b3120181b33816f84004bb662cf7027d2623fa75
Thomas O'Neill <tom@opensystemslab.io>
2023-06-25 16:01:18 +0100

    wip propped tree start

---
+++ app/design/ui-3d/propped/ProppedHouse.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { IndexedModel, IndexedModule } from "../../../db/system"
+import { House } from "../../../db/user"
+
+type Props = {
+  house: House
+  modules: IndexedModule[]
+  models: Record<string, IndexedModel[]>
+}
+
+const ProppedHouse = (props: Props) => {
+  const { house } = props
+  console.log(house)
+  return <group name={house.id}></group>
+}
+
+export default ProppedHouse
