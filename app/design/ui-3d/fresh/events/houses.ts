82ab69af6219fe32ac424606ed1b677bb3938c2a
Thomas O'Neill <tom@opensystemslab.io>
2023-10-11 09:44:05 +0100

    wip

--- app/design/ui-3d/fresh/events/houses.ts
+++ app/design/ui-3d/fresh/events/houses.ts
@@ -10,30 +10,7 @@ import { setRaycasting } from "../../../../utils/three"
 import { setSidebar } from "../../../state/settings"
 import { createHouseTransformsGroup } from "../scene/houseTransformsGroup"
 
-const ADD_HOUSE_INTENT_EVENT = "AddHouseIntentEvent"
-const ADD_HOUSE_EVENT = "AddHouseEvent"
 const REQUEST_SCENE_EVENT = "RequestSceneEvent"
-
-export const dispatchAddHouseIntent = (detail: HouseType) =>
-  dispatchEvent(
-    new CustomEvent(ADD_HOUSE_INTENT_EVENT, {
-      detail,
-    })
-  )
-
-export const useAddHouseIntentListener = (
-  f: (eventDetail: HouseType) => void
-) => useEvent(ADD_HOUSE_INTENT_EVENT, ({ detail }) => f(detail))
-
-export const dispatchAddHouse = (detail: House) =>
-  dispatchEvent(
-    new CustomEvent(ADD_HOUSE_EVENT, {
-      detail,
-    })
-  )
-
-export const useAddHouseListener = (f: (eventDetail: House) => void) =>
-  useEvent(ADD_HOUSE_EVENT, ({ detail }) => f(detail))
 
 export const useRequestSceneEventListener = (rootRef: RefObject<Group>) =>
   useEvent(REQUEST_SCENE_EVENT, () => O.fromNullable(rootRef.current))
