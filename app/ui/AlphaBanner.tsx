00d34f050a5645a74caf2d4b5cd1d1deb984be0c
Thomas O'Neill <tom@opensystemslab.io>
2023-11-30 09:42:58 +0000

    wip

--- app/ui/AlphaBanner.tsx
+++ app/ui/AlphaBanner.tsx
@@ -1,23 +1,35 @@
-import React from "react"
+import { Close } from "@carbon/icons-react"
+import { useState } from "react"
 
-const AlphaBanner: React.FC = () => {
-  return (
-    <div className="py-1.5 px-2 text-sm w-full bg-safety">
-      <span className="font-bold">ALPHA</span>
-      <span className="ml-2">This is a prototype.</span>
-      <span>
-        {`You can help us improve by giving `}
-        <a
-          className="underline"
-          href="https://form.typeform.com/to/inbsKUl2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          feedback and suggestions.
-        </a>
-      </span>
+const AlphaBanner = () => {
+  const [open, setOpen] = useState(true)
+
+  return open ? (
+    <div className="flex justify-between py-1.5 px-2 text-sm w-full bg-safety">
+      <div>
+        <span className="font-bold">ALPHA</span>
+        <span className="ml-2">This is a prototype.</span>
+        <span>
+          {`You can help us improve by giving `}
+          <a
+            className="underline"
+            href="https://form.typeform.com/to/inbsKUl2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            feedback and suggestions.
+          </a>
+        </span>
+      </div>
+      <div>
+        <span>
+          <button onClick={() => void setOpen(false)}>
+            <Close size={"16"} />
+          </button>
+        </span>
+      </div>
     </div>
-  )
+  ) : null
 }
 
 export default AlphaBanner
