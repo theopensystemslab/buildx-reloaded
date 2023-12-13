0eba23935ade615944c59433ccde356ef6bb323a
mysterybear <50906986+mysterybear@users.noreply.github.com>
2023-12-13 21:39:26 +0800

    feat: fathom (#95)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,21 @@
-import clsx from "clsx"
-import { Inter } from "next/font/google"
-import { PropsWithChildren } from "react"
-import "~/styles/globals.css"
-import Footer from "./ui/Footer"
-import Nav from "./ui/Nav"
+import clsx from "clsx";
+import { Inter } from "next/font/google";
+import { PropsWithChildren } from "react";
+import "~/styles/globals.css";
+import Footer from "./ui/Footer";
+import Nav from "./ui/Nav";
+import Fathom from "./Fathom";
 
 const inter = Inter({
   subsets: ["latin"],
   display: "swap",
-})
+});
 
 const Layout = ({ children }: PropsWithChildren<{}>) => {
   return (
     <html lang="en" className={clsx(inter.className, "w-full h-full")}>
       <body className="w-full h-full flex flex-col overflow-hidden">
+        <Fathom />
         <div className="flex-1 flex-grow-0">
           <Nav />
         </div>
@@ -25,7 +27,7 @@ const Layout = ({ children }: PropsWithChildren<{}>) => {
         </div>
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
