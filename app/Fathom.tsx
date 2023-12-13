da4b463295b87662ea818a897e929c8d2ca09b0a
Thomas O'Neill <tom@opensystemslab.io>
2023-12-13 21:30:03 +0800

    wip

--- app/Fathom.tsx
+++ app/Fathom.tsx
@@ -1,23 +1,39 @@
 "use client";
+
 import { load, trackPageview } from "fathom-client";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 
-const Fathom = () => {
+function TrackPageView() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
+  // Load the Fathom script on mount
   useEffect(() => {
-    load("FSLAOVYH", {
-      includedDomains: ["build.wikihouse.cc"],
+    load("MY_FATHOM_ID", {
+      auto: false,
     });
   }, []);
 
+  // Record a pageview when route changes
   useEffect(() => {
-    trackPageview();
-    // Record a pageview when route changes
+    if (!pathname) return;
+
+    trackPageview({
+      url: pathname + searchParams.toString(),
+      referrer: document.referrer,
+    });
   }, [pathname, searchParams]);
+
   return null;
+}
+
+const Fathom = () => {
+  return (
+    <Suspense fallback={null}>
+      <TrackPageView />
+    </Suspense>
+  );
 };
 
 export default Fathom;
