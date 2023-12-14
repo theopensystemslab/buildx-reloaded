d780dde6e17e5cb7e5ab94f0983e8d369233e2fb
Thomas O'Neill <tom@opensystemslab.io>
2023-12-14 15:33:57 +0800

    fix: enable controls in design view

--- app/design/app.tsx
+++ app/design/app.tsx
@@ -25,7 +25,7 @@ const App = () => {
   return houseTypes.length > 0 ? (
     <AppInit controlsEnabled={true} mapEnabled={false}>
       <Routing />
-      <FreshApp />
+      <FreshApp controlsEnabled />
     </AppInit>
   ) : (
     <Loader />
